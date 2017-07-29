68f4d45ddc4542d44d306eb97b557d8aa24cc192
puustina <anssi_puustinen@hotmail.com>
2017-07-29 22:47:49 +0300

    Added art

--- main.lua
+++ main.lua
@@ -3,6 +3,23 @@ local systems = require "src.systems"
 
 local entities = {}
 
+local bg = love.graphics.newImage("assets/bg.png")
+local timer = 0
+local whatToSpawn = {
+-- time | wavenumber | enemies
+	{0, {15,0,0}},
+	{10, {0,5,0}},
+	{15, {40,0,0}},
+	{20, {0,0,1}},
+	{30, {20,0,0}}
+}
+local spawnOutsideScreen = function(fun)
+	local wWidth, wHeight = love.window.getMode()
+	local angle = math.random(100*-math.pi, 100*math.pi)
+	local dist = math.sqrt(math.pow(wWidth/2, 2) + math.pow(wHeight/2, 2))
+	entities[#entities + 1] = fun(wWidth/2 + math.cos(angle)*dist, wHeight/2 + math.sin(angle)*dist)
+end
+
 function love.load()
 	math.randomseed(os.time())
 	
@@ -10,22 +27,30 @@ function love.load()
 
 	-- make player
 	entities[1] = prefabs.player(wWidth/2, wHeight/2)
-
-	local spawnOutsideScreen = function(fun)
-		local angle = math.random(100*-math.pi, 100*math.pi)
-		local dist = math.sqrt(math.pow(wWidth/2, 2) + math.pow(wHeight/2, 2))
-		entities[#entities + 1] = fun(wWidth/2 + math.cos(angle)*dist, wHeight/2 + math.sin(angle)*dist)
-	end
-
-	-- make enemies
-	for i = 1, 3, 1 do
-		spawnOutsideScreen(prefabs.peasant)
-	end
-	spawnOutsideScreen(prefabs.knight)
-	spawnOutsideScreen(prefabs.boss)
 end
 
 function love.update(dt)
+	timer = timer + dt
+
+	for i = #whatToSpawn, 1, -1 do
+		if whatToSpawn[i][1] < timer and whatToSpawn[i][2] then
+			for enemyIndex, count in ipairs(whatToSpawn[i][2]) do
+				for k = 1, count, 1 do
+					local fun = nil
+					if enemyIndex == 1 then
+						fun = prefabs.peasant
+					elseif enemyIndex == 2 then
+						fun = prefabs.knight
+					elseif enemyIndex == 3 then
+						fun = prefabs.boss
+					end
+					spawnOutsideScreen(fun)
+				end
+			end
+			whatToSpawn[i][2] = nil
+		end
+	end
+
 	for i, j in ipairs(entities) do
 		for k, sys in ipairs(systems.systems) do
 			sys(j, dt, entities)
@@ -41,8 +66,8 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.setBackgroundColor(255, 255, 255)
 	love.graphics.setColor(255, 255, 255)
+	love.graphics.draw(bg, 0, 0)
 	for i, j in ipairs(entities) do
 		for k, sys in ipairs(systems.drawSystems) do
 			sys(j)
